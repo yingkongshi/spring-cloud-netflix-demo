da11ef68f3b4713e96ba5e80a342e1c0351ade28
yingkongshi <lbo_lk@126.com>
2016-11-20 01:50:57 -0800

    finished eureka server Peer Awareness and other config

--- config-server/src/main/java/com/springcloud/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/springcloud/configserver/ConfigServerApplication.java
@@ -2,10 +2,12 @@ package com.springcloud.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
+@EnableEurekaClient
 @EnableConfigServer
 public class ConfigServerApplication {
 
